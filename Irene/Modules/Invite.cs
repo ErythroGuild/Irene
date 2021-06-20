aa25994823a63248454608c6d5bd5d5855a25a1c
Ernest Gu <ernest.gu.3.14@gmail.com>
2021-06-19 20:24:50 -0700

    Rewrite Invite module to implement IModule

---
+++ Irene/Modules/Invite.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using static Irene.Program;
+
+namespace Irene.Modules {
+	using id_ch = ChannelIDs;
+
+	class Invite : IModule {
+		readonly static List<string> token_erythro = new () {
+			"erythro", "ery", "wow"
+		};
+		readonly static List<string> token_leuko = new () {
+			"leuko", "ffxiv", "ff14"
+		};
+
+		const string invite_erythro = @"https://discord.gg/ADzEwNS";
+		const string invite_leuko   = @"https://discord.gg/zhadQf59xq";
+
+		public static string help() {
+			StringWriter text = new ();
+
+			text.WriteLine("`@Irene -inv erythro` fetches the server invite for this server,");
+			text.WriteLine("`@Irene -inv leuko` fetches the server invite for the FFXIV sister server,");
+			text.WriteLine("and `@Irene -inv` fetches both server invites.");
+			text.WriteLine($"These invite links can also be found in {channels[id_ch.resources]}.");
+
+			text.Flush();
+			return text.ToString();
+		}
+
+		public static void run_command(Command cmd) {
+			string arg = cmd.args.Trim().ToLower();
+
+			StringWriter text = new ();
+			if (token_erythro.Contains(arg)) {
+				text.WriteLine(invite_erythro);
+			} else if (token_leuko.Contains(arg)) {
+				text.WriteLine(invite_leuko);
+			} else {
+				text.WriteLine("**Erythro** and **Leuko** server invites:");
+				text.WriteLine(invite_erythro);
+				text.WriteLine(invite_leuko);
+			}
+
+			text.Flush();
+			_ = cmd.msg.RespondAsync(text.ToString());
+		}
+	}
+}
