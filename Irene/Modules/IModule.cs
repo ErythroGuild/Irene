a413e5b89bfb509be3de11bbced467f30d4e954c
Ernest Gu <ernest.gu.3.14@gmail.com>
2021-06-19 20:24:02 -0700

    Add a shared module interface

---
+++ Irene/Modules/IModule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Irene.Modules {
+	interface IModule {
+		public static string help()
+			{ throw new NotImplementedException(); }
+		public static void run_command(Command cmd)
+			{ throw new NotImplementedException(); }
+	}
+}
