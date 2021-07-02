83c733229c6d63d6fd7f3e01fc04990f1f419d05
Ernest Gu <ernest.gu.3.14@gmail.com>
2021-07-02 10:27:57 -0700

    Add Welcome module

---
+++ Irene/Modules/Welcome.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+using DSharpPlus.Entities;
+
+using static Irene.Program;
+
+namespace Irene.Modules {
+	using id_r = RoleIDs;
+	using id_ch = ChannelIDs;
+
+	static class Welcome {
+		const string path_message = @"data/welcome.txt";
+		const string url_mascot = @"https://imgur.com/5pKJdPh";
+		const ulong ch_notify = id_ch.officerBots;
+		const ulong r_recruiter = id_r.recruiter;
+
+		// Force static initializer to run.
+		public static void init() { return; }
+
+		static Welcome() {
+			irene.GuildMemberAdded += (irene, e) => {
+				_ = Task.Run(async () => {
+					DiscordMember member = e.Member;
+
+					// Initialize welcome message.
+					StreamReader data = File.OpenText(path_message);
+					string welcome = data.ReadToEnd();
+					data.Close();
+
+					// Send welcome message to new member.
+					log.info("Sending welcome message to new member.");
+					log.debug($"  {member.Username}#{member.Discriminator}");
+					await member.SendMessageAsync(welcome);
+					await member.SendMessageAsync(url_mascot);
+
+					// Notify recruitment officer.
+					if (is_guild_loaded) {
+						log.debug("  Notifying recruitment officer.");
+						StringWriter text = new ();
+						text.WriteLine($"{roles[r_recruiter].Mention} - " +
+							$"New member ({e.Member.Mention}) joined.");
+						text.Flush();
+						_ = channels[ch_notify].SendMessageAsync(text.ToString());
+					}
+				});
+				return Task.CompletedTask;
+			};
+		}
+	}
+}
