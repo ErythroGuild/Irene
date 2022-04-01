bdebd3e842ccdeb8286c2c06da6e6aac1c83aaf5
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-03-31 22:52:21 -0700

    Add a Stopwatch logging Util

--- Irene/GlobalUsings.cs
+++ Irene/GlobalUsings.cs
@@ -1,6 +1,7 @@
-﻿// System.
+// System.
 global using System;
 global using System.Collections.Generic;
+global using System.Diagnostics;
 global using System.IO;
 global using System.Threading.Tasks;
 
---
+++ Irene/Utils/Timers.cs
@@ -0,0 +1,23 @@
+﻿namespace Irene.Utils;
+
+static partial class Util {
+	// Convenience  methods for stopping a timer and printing the value.
+	public static void LogMsecDebug(this Stopwatch stopwatch, string template, bool doStopTimer=true) {
+		if (doStopTimer)
+			stopwatch.Stop();
+		long msec = stopwatch.ElapsedMilliseconds;
+		Log.Debug(template, msec);
+	}
+	public static void LogMsecInformation(this Stopwatch stopwatch, string template, bool doStopTimer=true) {
+		if (doStopTimer)
+			stopwatch.Stop();
+		long msec = stopwatch.ElapsedMilliseconds;
+		Log.Information(template, msec);
+	}
+	public static void LogMsecWarning(this Stopwatch stopwatch, string template, bool doStopTimer=true) {
+		if (doStopTimer)
+			stopwatch.Stop();
+		long msec = stopwatch.ElapsedMilliseconds;
+		Log.Warning(template, msec);
+	}
+}
