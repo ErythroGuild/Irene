9145ca2180added162612b4f83d5a49e6821c038
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-09-22 16:58:36 -0700

    [WIP] Add utils for mention formatting from ID alone

--- Irene/Utils/DiscordFormat.cs
+++ Irene/Utils/DiscordFormat.cs
@@ -21,6 +21,11 @@ static partial class Util {
 	public static string Mention(this DiscordApplicationCommand command, string display) =>
 		$"</{display}:{command.Id}>";
 
+	// Mention formatting.
+	public static string MentionUserId(this ulong id) => $"<@{id}>";
+	public static string MentionChannelId(this ulong id) => $"<#{id}>";
+	public static string MentionRoleId(this ulong id) => $"<@&{id}>";
+
 	// Adjusts to 12-/24-hour clock based on user's client locale.
 	// Default style (no format specifier) is equivalent to ":f"
 	// (DateTimeShort).
