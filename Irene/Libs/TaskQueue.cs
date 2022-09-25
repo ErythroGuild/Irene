f82fda605c32bd13d2737081dea89389e0f22ffb
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-09-25 16:40:11 -0700

    [WIP] Add lightweight `TaskQueue` class (closes #244)

---
+++ Irene/Libs/TaskQueue.cs
@@ -0,0 +1,29 @@
+﻿namespace Irene;
+
+// A lightweight, thread-safe class, which executes queued tasks FIFO.
+class TaskQueue {
+	public bool IsRunning => !_task.IsCompleted;
+
+	private readonly ConcurrentQueue<Task> _queue = new ();
+	private Task _task = Task.CompletedTask; // initialize as available
+
+	public void Add(Task action) {
+		_queue.Enqueue(action);
+
+		if (!IsRunning) {
+			_task = Task.Run(async () => {
+				while (!_queue.IsEmpty) {
+					_queue.TryDequeue(out Task? task);
+
+					if (task is null)
+						continue;
+
+					if (task.Status == TaskStatus.Created)
+						task.Start();
+
+					await task;
+				}
+			});
+		}
+	}
+}
