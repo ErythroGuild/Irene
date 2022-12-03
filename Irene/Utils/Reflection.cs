e68af84ddd23a889bf03c80250ac5f5a05caee1f
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-12-02 17:25:51 -0800

    [WIP] Move method for invoking all static ctors to Utils

--- Irene/Utils/Reflection.cs
+++ Irene/Utils/Reflection.cs
@@ -1,6 +1,7 @@
 ﻿namespace Irene.Utils;
 
 using System.Diagnostics;
+using System.Reflection;
 
 static partial class Util {
 	// Whether or not the current program is a debug build.
@@ -17,4 +18,37 @@ static partial class Util {
 	// Convenience method for checking interfaces during reflection.
 	public static bool ImplementsInterface(this Type type, Type @interface) =>
 		new List<Type>(type.GetInterfaces()).Contains(@interface);
+
+	// Runs all static constructors, for any type in the specified set
+	// that has them.
+	public static void RunAllStaticConstructors(IReadOnlySet<Type> types) {
+		foreach (Type type in types) {
+			// See Microsoft's documentation for `GetConstructors()`.
+			// These flags are the exact ones needed to fetch static
+			// constructors.
+			List<ConstructorInfo> constructors = new (
+					type.GetConstructors(
+						BindingFlags.Public |
+						BindingFlags.Static |
+						BindingFlags.NonPublic |
+						BindingFlags.Instance
+					)
+				);
+
+			// Note: calling the constructor directly risks calling
+			// the static constructor more than once. Instead, check
+			// for the presence of a static constructor, and if one
+			// exists, we use the following method to call it (if it
+			// hasn't been called yet), and indicate to the runtime
+			// that it _has_ been called.
+			foreach (ConstructorInfo constructor in constructors) {
+				if (constructor.IsStatic) {
+					System.Runtime.CompilerServices
+						.RuntimeHelpers
+						.RunClassConstructor(type.TypeHandle);
+					break;
+				}
+			}
+		}
+	}
 }
