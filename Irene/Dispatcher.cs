b381aec315a5b47d593eb7cfe1699c9164a80eb4
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-12-02 23:06:13 -0800

    [WIP] Rename (and rewrite) `CommandDispatcher`
    especially now that the old `Command` class has been removed; it doesn't actually make sense to rename this to `Command` (because that's not what it is).

---
+++ Irene/Dispatcher.cs
@@ -0,0 +1,51 @@
+﻿namespace Irene;
+
+using System.Reflection;
+
+static class Dispatcher {
+	public static IReadOnlyDictionary<string, CommandHandler> Table { get; private set; }
+	public static IReadOnlyList<string> CommandNames =>
+		new List<string>(Table.Keys);
+	public static IReadOnlyList<CommandHandler> Handlers =>
+		new List<CommandHandler>(Table.Values);
+
+	static Dispatcher() =>
+		Table = new ConcurrentDictionary<string, CommandHandler>();
+
+	// This replaces the entire internal handler table with a snapshot
+	// of the handlers evaluated at call time. This is called by the static
+	// initializer, but can also be manually invoked.
+	public static void ReplaceAllHandlers() {
+		ConcurrentDictionary<string, CommandHandler> handlerTable = new ();
+
+		List<Type> types = new (Assembly.GetExecutingAssembly().GetTypes());
+		foreach (Type type in types) {
+			if (type.BaseType != typeof(CommandHandler))
+				continue;
+
+			// Check for the default (no args) constructor.
+			ConstructorInfo? constructor =
+				type.GetConstructor(Array.Empty<Type>());
+			if (constructor is null) {
+				Log.Error("  Could not find default constructor for CommandHandler class: {ClassName}", type.FullName);
+				continue;
+			}
+
+			// Create an handler instance to register to the lookup table.
+			CommandHandler handler =
+				(CommandHandler)constructor
+				.Invoke(Array.Empty<object>());
+			handlerTable.TryAdd(handler.Command.Name, handler);
+		}
+
+		Log.Debug("  Added {HandlerCount} commands to Dispatcher.", handlerTable.Count);
+		Table = handlerTable;
+	}
+
+	public static bool CanHandle(string commandName) =>
+		Table.ContainsKey(commandName);
+	public static Task HandleAsync(string commandName, Interaction interaction) =>
+		Table.ContainsKey(commandName)
+			? Table[commandName].HandleAsync(interaction)
+			: throw new ArgumentException("Unregistered command.", nameof(commandName));
+}
