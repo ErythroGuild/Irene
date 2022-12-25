e60f5c19e7e2a7750f6cc2e8b62a18a54d1ef28d
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-12-25 03:04:08 -0800

    Tweak `/about` "avail. commands" wording

--- Irene/Modules/About.cs
+++ Irene/Modules/About.cs
@@ -78,7 +78,7 @@ class About {
 			$"""
 			{Erythro.Emoji(id_e.erythro)} **<Erythro>**'s community admin bot.
 
-			{statusAvailableCommands} **Available commands:** {SlashCommandCount} [+{ContextCommandCount}], {helpLink}
+			{statusAvailableCommands} **Usable commands:** {SlashCommandCount} [+{ContextCommandCount}], {helpLink}
 			{statusMemoryUsage} **Memory usage:** {GetMemoryUsageMB():F0} MB
 
 			Maintained by {GetMaintainerMention()} with {_charLove}
