4fd97a16dfed96850ba7cd50e8fa45cfc5dfe35d
Ernest Gu <ernest.gu.3.14@gmail.com>
2023-01-04 13:07:37 -0800

    Fix `Dispatcher.Mention()` implementation
    technically part of #476

--- Irene/Dispatcher.cs
+++ Irene/Dispatcher.cs
@@ -15,8 +15,11 @@ static class Dispatcher {
 		Table = new ConcurrentDictionary<string, CommandHandler>();
 
 	// Requires the command to be registered (`ReplaceAllHandlers()`).
-	public static string Mention(string command, params string[] subcommands) =>
-		Table[command].Mention(string.Join(" ", command, subcommands));
+	public static string Mention(string command, params string[] subcommands) {
+		List<string> tokens = new () { command };
+		tokens.AddRange(subcommands);
+		return Table[command].Mention(string.Join(" ", tokens));
+	}
 
 	public static bool CanHandle(string commandName) =>
 		Table.ContainsKey(commandName);
