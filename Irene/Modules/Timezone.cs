bcb200c2990d6ad8f2db08f43646d0e021053263
Ernest Gu <ernest.gu.3.14@gmail.com>
2023-01-10 04:47:42 -0800

    [DEBUG] Change `Timezone` stub to use known good values

--- Irene/Modules/Timezone.cs
+++ Irene/Modules/Timezone.cs
@@ -2,14 +2,22 @@
 
 class Timezone {
 	public static readonly TimeZoneInfo
-		TimeZone_PST = TimeZoneInfo.FindSystemTimeZoneById("PST"),
-		TimeZone_PDT = TimeZoneInfo.FindSystemTimeZoneById("Etc/GMT+7"),
-		TimeZone_MST = TimeZoneInfo.FindSystemTimeZoneById("MST"),
-		TimeZone_MDT = TimeZoneInfo.FindSystemTimeZoneById("Etc/GMT+6"),
-		TimeZone_CST = TimeZoneInfo.FindSystemTimeZoneById("CST"),
-		TimeZone_CDT = TimeZoneInfo.FindSystemTimeZoneById("Etc/GMT+5"),
-		TimeZone_EST = TimeZoneInfo.FindSystemTimeZoneById("EST"),
-		TimeZone_EDT = TimeZoneInfo.FindSystemTimeZoneById("Etc/GMT+4");
+		//TimeZone_PST = TimeZoneInfo.FindSystemTimeZoneById("PST"),
+		//TimeZone_PDT = TimeZoneInfo.FindSystemTimeZoneById("Etc/GMT+7"),
+		//TimeZone_MST = TimeZoneInfo.FindSystemTimeZoneById("MST"),
+		//TimeZone_MDT = TimeZoneInfo.FindSystemTimeZoneById("Etc/GMT+6"),
+		//TimeZone_CST = TimeZoneInfo.FindSystemTimeZoneById("CST"),
+		//TimeZone_CDT = TimeZoneInfo.FindSystemTimeZoneById("Etc/GMT+5"),
+		//TimeZone_EST = TimeZoneInfo.FindSystemTimeZoneById("EST"),
+		//TimeZone_EDT = TimeZoneInfo.FindSystemTimeZoneById("Etc/GMT+4");
+		TimeZone_PST = TimeZoneInfo.FindSystemTimeZoneById("America/Denver"),
+		TimeZone_PDT = TimeZoneInfo.FindSystemTimeZoneById("America/Denver"),
+		TimeZone_MST = TimeZoneInfo.FindSystemTimeZoneById("America/Denver"),
+		TimeZone_MDT = TimeZoneInfo.FindSystemTimeZoneById("America/Denver"),
+		TimeZone_CST = TimeZoneInfo.FindSystemTimeZoneById("America/Denver"),
+		TimeZone_CDT = TimeZoneInfo.FindSystemTimeZoneById("America/Denver"),
+		TimeZone_EST = TimeZoneInfo.FindSystemTimeZoneById("America/Denver"),
+		TimeZone_EDT = TimeZoneInfo.FindSystemTimeZoneById("America/Denver");
 
 	public static TimeZoneInfo? GetUserLocalTime(DiscordUser user) =>
 		GetUserLocalTime(user.Id);
